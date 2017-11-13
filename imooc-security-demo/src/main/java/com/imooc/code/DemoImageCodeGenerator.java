9d1e5a7dbbab871b10a6ae2b8ea1f74a0c203b6f
dengrenbocq <dengrenbo@outlook.com>
2017-11-13 14:12:58 +0800

    验证码重构1

--- imooc-security-demo/src/main/java/com/imooc/code/DemoImageCodeGenerator.java
+++ imooc-security-demo/src/main/java/com/imooc/code/DemoImageCodeGenerator.java
@@ -8,11 +8,10 @@ import org.springframework.web.context.request.ServletWebRequest;
 /**
  * Created by 邓仁波 on 2017-11-9.
  */
-@Component("imageValidateCodeGenerator")
+//@Component("imageValidateCodeGenerator")
 public class DemoImageCodeGenerator implements ValidateCodeGenerator {
     @Override
     public ImageCode generate(ServletWebRequest request) {
-
         System.out.println("生成图像验证码");
         return null;
     }
