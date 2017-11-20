7096165624217b79e62bb8946a775a2df6ed0809
dengrenbocq <dengrenbo@outlook.com>
2017-11-20 15:13:32 +0800

    6-2

---
+++ imooc-security-app/src/main/java/ImoocAuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by 邓仁波 on 2017-11-20.
+ */
+@CacheConfig
+@EnableAuthorizationServer
+public class ImoocAuthorizationServerConfig {
+}
--- imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
+++ imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
@@ -2,13 +2,13 @@ package com.imooc.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.security.SocialUser;
+import org.springframework.social.security.SocialUserDetails;
+import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,29 +16,45 @@ import org.springframework.stereotype.Component;
  * 重写登录验证
  */
 @Component
-public class MyUserDetailsService implements UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService, SocialUserDetailsService {
     private Logger logger = LoggerFactory.getLogger(getClass());
-//    @Autowired
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("表单登录用户名:" + username);
+        return buildUser(username);
+    }
+
+    @Override
+    public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
+        logger.info("设计登录用户Id:" + userId);
+        return buildUser(userId);
+    }
+
+
+    //    @Autowired
     //注册账号的时候用 passwordEncoder.encode() 加密
     //验证的时候 loadUserByUsername password直接放入加密前的 字符即可 不用自己解密
 //    private PasswordEncoder passwordEncoder;
 
     /**
      * 根据用户名获取用户信息
+     *
      * @param username
      * @return
      * @throws UsernameNotFoundException 用户不存在
      */
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    private SocialUserDetails buildUser(String username) {
         logger.info("登录用户名: " + username);
         //可以注入jdbc redis等 获取用户详细信息
         //根据查找到的用户信息判断账号是否被冻结
 
         //$2a$10$2pnnzjVevYW0A.DAD2tfUO.RVzesPqEwmc/IJC896x9kqeqd.jNsS 为你数据库密码
-        return new User(username, "$2a$10$2pnnzjVevYW0A.DAD2tfUO.RVzesPqEwmc/IJC896x9kqeqd.jNsS"
+        return new SocialUser(username, "$2a$10$2pnnzjVevYW0A.DAD2tfUO.RVzesPqEwmc/IJC896x9kqeqd.jNsS"
                 //账号可用，账号没过期,凭证没过期,账号没被锁定
                 , false, true, true, true
                 , AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
+
 }
