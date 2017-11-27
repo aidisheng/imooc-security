b0a19915eb8858da924fb693e54f892c864e6e15
dengrenbocq <dengrenbo@outlook.com>
2017-11-27 15:21:50 +0800

    5-5 qq登录

--- imooc-security-core/src/main/java/com/imooc/security/core/social/qq/api/QQImpl.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/social/qq/api/QQImpl.java
@@ -2,16 +2,17 @@ package com.imooc.security.core.social.qq.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
 import org.springframework.social.oauth2.TokenStrategy;
-
-import java.io.IOException;
 
 /**
  * Created by 邓仁波 on 2017-11-23.
  * 获取用户信息
  */
 public class QQImpl extends AbstractOAuth2ApiBinding implements QQ {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     private String appId;
     private String openId;
     private static final String URL_GET_OPENID = "https://graph.qq.com/oauth2.0/me?access_token=%s";
@@ -24,19 +25,22 @@ public class QQImpl extends AbstractOAuth2ApiBinding implements QQ {
         this.appId = appId;
         String url = String.format(URL_GET_OPENID, accessToken);
         String result = getRestTemplate().getForObject(url, String.class);
-        System.out.println(result);
-        this.openId = StringUtils.substringBetween(result, "\"openid\":", "}");
+        logger.info(result);
+        this.openId = StringUtils.substringBetween(result, "\"openid\":\"", "\"}");
     }
 
     @Override
     public QQUserInfo getUserInfo() {
         String url = String.format(URL_GET_USERINFO, appId, openId);
         String result = getRestTemplate().getForObject(url, String.class);
-        System.out.println(result);
+        logger.info(result);
+        QQUserInfo userInfo = null;
         try {
-            return objectMapper.readValue(result, QQUserInfo.class);
-        } catch (IOException e) {
-            throw new RuntimeException("获取用户信息失败！");
+            userInfo = objectMapper.readValue(result, QQUserInfo.class);
+            userInfo.setOpenId(openId);
+            return userInfo;
+        } catch (Exception e) {
+            throw new RuntimeException("获取用户信息失败", e);
         }
     }
 
